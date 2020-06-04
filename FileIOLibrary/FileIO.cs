e945f3b477020b3324156f908ae184a4f082d3cf
Jhonathan Araujo <JhonathanAraujoBR@Hotmail.com>
2020-06-04 16:09:56 -0300

    Usando Class Library DLL

---
+++ FileIOLibrary/FileIO.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace FileIOLibrary
+{
+    public class FileIO
+    {
+        public string ReadLine(string filePath, int lineNumber)
+        {
+            return ReadSpecificLine(filePath, lineNumber);
+        }
+
+        private string ReadSpecificLine(string filePath, int lineNumber)
+        {
+            string content = null;
+
+            try
+            {
+                /* StreamReader: pode ser usado para ler o conteúdo de um arquivo de texto. Possui métodos para ler caracteres individuais, ler até o final do arquivo e ler linhas individuais.
+                 * 
+                 */
+
+                using (StreamReader inputFile = new StreamReader(filePath))
+                {
+                    // Pulando linhas até chegar na escolhida
+                    for (int i = 1; i < lineNumber; i++)
+                    {
+                        // ReadLine (): lê uma única linha de um fluxo e retorna o conteúdo dessa linha como uma string. Especificamente, ele lê a linha correspondente à posição atual do ponteiro de fluxo.
+                        inputFile.ReadLine();
+
+                        if (inputFile.EndOfStream)
+                        {
+                            Console.WriteLine($"Fim. Arquivo contém somente {i} linhas.");
+                            break;
+                        }
+                    }
+
+                    // Linha especifica para leitura
+                    content = inputFile.ReadLine();
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Erro ao ler o arquivo...");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERRO...\n" + ex.Message);
+            }
+
+            return content;
+        }
+    }
+}
---
+++ UsandoDLL/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using FileIOLibrary;
+
+namespace UsandoDLL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "ReadFile.txt");
+            string lineContents = null;
+            int lineNumber = 3;
+
+            FileIO fileIO = new FileIO();
+            lineContents = fileIO.ReadLine(filePath, lineNumber);
+
+            if (lineContents != null)
+                Console.WriteLine(lineContents);
+
+            Console.ReadLine();
+        }
+    }
+}
